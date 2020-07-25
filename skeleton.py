3a815a159bc673d76b93e2f4b7f91852fa860881
Gopipatwa <52290012+Gopipatwa@users.noreply.github.com>
2020-07-25 18:25:41 +0530

    Add files via upload

---
+++ skeleton.py
@@ -0,0 +1,306 @@
+import random
+import pyfiglet
+import time
+def skeleton(value):
+
+    for i in range(150):
+        if i>44 and i<86:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>40 and i<90:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>38 and i<92:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>36 and i<94:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>34 and i<96:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>33 and i<97:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>32 and i<98:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>31 and i<99:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>32 and i<98:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>33 and i<97:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>34 and i<96:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>35 and i<95:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    #eyes
+
+    for i in range(150):
+        if (i>36 and i<44):
+            print(value, end="")
+        elif(i>63 and i<67):
+            print(value,end="")
+        elif(i>86 and i<94):
+            print(value,end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if (i>35 and i<45):
+            print(value, end="")
+        elif(i>61 and i<69):
+            print(value,end="")
+        elif(i>85 and i<95):
+            print(value,end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if (i>34 and i<45):
+            print(value, end="")
+        elif(i>59 and i<71):
+            print(value,end="")
+        elif(i>85 and i<96):
+            print(value,end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if (i>33 and i<46):
+            print(value, end="")
+        elif(i>57 and i<73):
+            print(value,end="")
+        elif(i>84 and i<97):
+            print(value,end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    #nose
+    for i in range(150):
+        if i>33 and i<62:
+            print(value, end="")
+        elif i>63 and i<67:
+            print(value,end="")
+        elif i>69 and i<97:
+            print(value,end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i > 34 and i < 62:
+            print(value, end="")
+        elif i > 63 and i < 67:
+            print(value, end="")
+        elif i > 69 and i < 96:
+            print(value, end="")
+        else:
+            print(" ", end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i > 35 and i < 62:
+            print(value, end="")
+        elif i > 63 and i < 67:
+            print(value, end="")
+        elif i > 69 and i < 95:
+            print(value, end="")
+        else:
+            print(" ", end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i > 36 and i < 59:
+            print(value, end="")
+        elif i > 63 and i < 67:
+            print(value, end="")
+        elif i > 72 and i < 94:
+            print(value, end="")
+        else:
+            print(" ", end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>38 and i<92:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+
+    for i in range(150):
+        if i>42 and i<88:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+#mouth
+
+    for i in range(150):
+        if i>41 and i<51:
+            print(value, end="")
+        elif i>54 and i<57:
+            print(value,end="")
+        elif i>60 and i<63:
+            print(value,end="")
+        elif i>66 and i<69:
+            print(value,end="")
+        elif i>72 and i<75:
+            print(value,end="")
+        elif i>79 and i<89:
+            print(value,end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>42 and i<51:
+            print(value, end="")
+        elif i>54 and i<57:
+            print(value,end="")
+        elif i>60 and i<63:
+            print(value,end="")
+        elif i>66 and i<69:
+            print(value,end="")
+        elif i>72 and i<75:
+            print(value,end="")
+        elif i>79 and i<88:
+            print(value,end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>43 and i<52:
+            print(value, end="")
+        elif i>78 and i<87:
+            print(value,end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>44 and i<53:
+            print(value, end="")
+        elif i>54 and i<57:
+            print(value,end="")
+        elif i>60 and i<63:
+            print(value,end="")
+        elif i>66 and i<69:
+            print(value,end="")
+        elif i>72 and i<75:
+            print(value,end="")
+        elif i>77 and i<86:
+            print(value,end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    #knee
+    for i in range(150):
+        if i>46 and i<84:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>50 and i<80:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+    for i in range(150):
+        if i>54 and i<76:
+            print(value, end="")
+        else:
+            print(" ",end="")
+    print()
+    time.sleep(sec)
+pink = "\033[95m"
+red = "\033[91m"
+blue = "\033[94m"
+lightblue = "\033[96m"
+green = "\033[92m"
+yellow = "\033[93m"
+redb = "\033[101m"
+pinkb = "\033[105m"
+blueb = "\033[104m"
+greenb = "\033[102m"
+yellowb = "\033[103m"
+lightblueb = "\033[106m"
+#black="\033[90m
+#color=[pink,red,blue,green,lightblue,yellow]
+#bcolor=[pinkb,redb,greenb,yellowb,lightblueb,blueb]
+
+if __name__=="__main__":
+    while True:
+        value=input("Enter Single character and N for exists:")
+        if value=="N"or value=="n":
+            break
+        else:
+            sec = 0.1
+            skeleton(yellow+value)
+            name=pyfiglet.figlet_format("SKELETON-HEAD\nBlackCoder")
+            print(red+name)
+
